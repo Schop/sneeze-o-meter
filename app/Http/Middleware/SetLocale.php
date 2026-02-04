6ba146e0cf9c6ad010d391d499874c17c3c9edfc
John Schop <jschop@vmi-group.com>
2026-02-04 20:01:23 +0100

    Change SetLocale middleware to use cookies instead of session to avoid session dependency

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetLocale
@@ -13,8 +14,8 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (session()->has('locale')) {
-            $locale = session('locale');
+        if (Cookie::has('locale')) {
+            $locale = Cookie::get('locale');
             if (in_array($locale, ['en', 'nl'])) {
                 app()->setLocale($locale);
             }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SneezeController;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -12,7 +13,7 @@ Route::get('/api/leaderboard-data', [HomeController::class, 'getLeaderboardData'
 // Language switcher
 Route::get('/language/{locale}', function ($locale) {
     if (in_array($locale, ['en', 'nl'])) {
-        session(['locale' => $locale]);
+        Cookie::queue('locale', $locale, 60*24*365); // 1 year
         app()->setLocale($locale);
     }
     return redirect()->back();
